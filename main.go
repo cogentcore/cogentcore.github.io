df30b81ba43432a76fccdef423710e0b14009a23
Kai O'Reilly <koreilly5297@gmail.com>
2024-01-16 22:38:56 -0800

    added about label

--- main.go
+++ main.go
@@ -9,5 +9,6 @@ import "cogentcore.org/core/gi"
 func main() {
 	b := gi.NewAppBody("Cogent Core")
 	gi.NewLabel(b).SetType(gi.LabelHeadlineLarge).SetText("Cogent Core")
+	gi.NewLabel(b).SetText("A free and open source software ecosystem for all platforms, built around a powerful, fast, and cogent core framework allowing you to Code Once, Run Everywhere.")
 	b.NewWindow().Run().Wait()
 }
