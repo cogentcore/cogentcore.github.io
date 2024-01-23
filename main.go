9e9a05e5c30ed636e86244146c2cb0844f7c15d2
Kai O'Reilly <koreilly5297@gmail.com>
2024-01-23 15:31:14 -0800

    use StartMainWindow

--- main.go
+++ main.go
@@ -10,5 +10,5 @@ func main() {
 	b := gi.NewAppBody("Cogent Core")
 	gi.NewLabel(b).SetType(gi.LabelHeadlineLarge).SetText("Cogent Core")
 	gi.NewLabel(b).SetText("A free and open source software ecosystem for all platforms, built around a powerful, fast, and cogent core framework allowing you to Code Once, Run Everywhere.")
-	b.NewWindow().Run().Wait()
+	b.StartMainWindow()
 }
