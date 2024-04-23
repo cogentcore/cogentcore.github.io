04f44fdc1526cfd683b94af2d1ece48d7c81a6ad
Kai O'Reilly <koreilly5297@gmail.com>
2024-04-23 12:49:50 -0700

    update to core Label to Text rename

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import "cogentcore.org/core/core"
 
 func main() {
 	b := core.NewBody("Cogent Core")
-	core.NewLabel(b).SetType(core.LabelHeadlineLarge).SetText("Cogent Core")
-	core.NewLabel(b).SetText("A free and open source software ecosystem for all platforms, built around a powerful, fast, and cogent core framework allowing you to Code Once, Run Everywhere.")
-	core.NewLabel(b).SetText(`See <a href="https://cogentcore.org/core">core</a> for more information.`)
+	core.NewText(b).SetType(core.TextHeadlineLarge).SetText("Cogent Core")
+	core.NewText(b).SetText("A free and open source software ecosystem for all platforms, built around a powerful, fast, and cogent core framework allowing you to Code Once, Run Everywhere.")
+	core.NewText(b).SetText(`See <a href="https://cogentcore.org/core">core</a> for more information.`)
 	b.RunMainWindow()
 }
