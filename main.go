5beb3c3a678473ddf062d2e0b2045fdb9116cc4e
Kai O'Reilly <koreilly5297@gmail.com>
2024-04-25 22:43:51 -0700

    replace cogent with elegant for adjectives

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import "cogentcore.org/core/core"
 func main() {
 	b := core.NewBody("Cogent Core")
 	core.NewText(b).SetType(core.TextHeadlineLarge).SetText("Cogent Core")
-	core.NewText(b).SetText("A free and open source software ecosystem for all platforms, built around a powerful, fast, and cogent core framework allowing you to Code Once, Run Everywhere.")
+	core.NewText(b).SetText("A free and open source software ecosystem for all platforms, built around a powerful, fast, and elegant framework allowing you to Code Once, Run Everywhere.")
 	core.NewText(b).SetText(`See <a href="https://cogentcore.org/core">core</a> for more information.`)
 	b.RunMainWindow()
 }
