025ae18d0b6e0c0a7b31f0869a2d6e0c21b35e74
Kai O'Reilly <koreilly5297@gmail.com>
2024-07-13 22:36:35 -0700

    start on pages based structure

---
+++ content/index.md
@@ -0,0 +1,1 @@
+<home-page></home-page>
--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 	"cogentcore.org/core/base/errors"
 	"cogentcore.org/core/core"
 	"cogentcore.org/core/events"
+	"cogentcore.org/core/htmlcore"
+	"cogentcore.org/core/pages"
 	"cogentcore.org/core/styles"
 	"cogentcore.org/core/styles/units"
 )
@@ -17,30 +19,38 @@ import (
 //go:embed name.png
 var resources embed.FS
 
+//go:embed content
+var content embed.FS
+
 func main() {
 	b := core.NewBody("Cogent Core")
+	pg := pages.NewPage(b).SetContent(content)
+	b.AddAppBar(pg.MakeToolbar)
 
-	frame := core.NewFrame(b)
-	frame.Styler(func(s *styles.Style) {
-		s.Direction = styles.Column
-		s.Grow.Set(1, 1)
-		s.CenterAll()
-	})
-	errors.Log(core.NewSVG(frame).ReadString(core.AppIcon))
-	img := core.NewImage(frame)
-	errors.Log(img.OpenFS(resources, "name.png"))
-	img.Styler(func(s *styles.Style) {
-		x := func(uc *units.Context) float32 {
-			return min(uc.Dp(612), uc.Vw(90))
-		}
-		s.Min.Set(units.Custom(x), units.Custom(func(uc *units.Context) float32 {
-			return x(uc) * (128.0 / 612.0)
-		}))
-	})
-	core.NewText(frame).SetType(core.TextHeadlineMedium).SetText(core.AppAbout)
-	core.NewButton(frame).SetText("Learn about the Cogent Core framework").OnClick(func(e events.Event) {
-		core.TheApp.OpenURL("https://cogentcore.org/core")
-	})
+	htmlcore.ElementHandlers["home-page"] = func(ctx *htmlcore.Context) bool {
+		frame := core.NewFrame(ctx.BlockParent)
+		frame.Styler(func(s *styles.Style) {
+			s.Direction = styles.Column
+			s.Grow.Set(1, 1)
+			s.CenterAll()
+		})
+		errors.Log(core.NewSVG(frame).ReadString(core.AppIcon))
+		img := core.NewImage(frame)
+		errors.Log(img.OpenFS(resources, "name.png"))
+		img.Styler(func(s *styles.Style) {
+			x := func(uc *units.Context) float32 {
+				return min(uc.Dp(612), uc.Vw(90))
+			}
+			s.Min.Set(units.Custom(x), units.Custom(func(uc *units.Context) float32 {
+				return x(uc) * (128.0 / 612.0)
+			}))
+		})
+		core.NewText(frame).SetType(core.TextHeadlineMedium).SetText(core.AppAbout)
+		core.NewButton(frame).SetText("Learn about the Cogent Core framework").OnClick(func(e events.Event) {
+			core.TheApp.OpenURL("https://cogentcore.org/core")
+		})
+		return true
+	}
 
 	b.RunMainWindow()
 }
