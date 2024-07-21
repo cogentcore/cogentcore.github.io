d5796c5de8a4ccdc9bbd7a938c80e2158d929fd5
Kai O'Reilly <koreilly5297@gmail.com>
2024-07-21 13:54:05 -0700

    remove icon which is no longer necessary since it is the default

--- main.go
+++ main.go
@@ -68,7 +68,7 @@ func main() {
 		errors.Log(img.OpenFS(resources, "name.png"))
 		img.Styler(func(s *styles.Style) {
 			x := func(uc *units.Context) float32 {
-				return min(uc.Dp(612), uc.Vw(90))
+				return min(uc.Dp(612), uc.Vw(80))
 			}
 			s.Min.Set(units.Custom(x), units.Custom(func(uc *units.Context) float32 {
 				return x(uc) * (128.0 / 612.0)
